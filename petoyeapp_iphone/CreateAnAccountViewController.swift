994d761d6df10be16d383abbc87dc7cc8808e9f2
Ameya <ameyavi@gmail.com>
2016-07-28 21:23:44 +0530

    fb signup done, creating a user using post to api done

--- petoyeapp_iphone/CreateAnAccountViewController.swift
+++ petoyeapp_iphone/CreateAnAccountViewController.swift
@@ -11,7 +11,44 @@ class CreateAnAccountViewController: UIViewController {
     
     @IBAction func email_signup(sender: AnyObject) {
         
-        getPetOye()
+        //getPetOye()
+        
+        var em = email.text
+        var pass = password.text
+        var conf = confirm.text
+        
+        if (pass == conf && em != nil) && (pass != nil && conf != nil)
+        {
+            let request = NSMutableURLRequest(URL: NSURL(string: "http://api.petoye.com/users/signup")!)
+            request.HTTPMethod = "POST"
+            let postString = "email=\(em).com&password=\(pass)"
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+                guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                    print(error!)
+                    return
+                }
+                
+                if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 201 {           // check for http errors
+                    print("statusCode should be 201, but is \(httpStatus.statusCode)")
+                    print(response!)
+                }
+                
+                var responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print(responseString!)
+                
+            }
+            task.resume()
+            
+        }
+        else
+        {
+            
+            //display password doesn't match
+            print("Password don't match")
+            
+        }
+
         
     }
     
@@ -47,6 +84,30 @@ class CreateAnAccountViewController: UIViewController {
     
     func postPetOye()
     {
+        
+        func postPetOye()
+        {
+            let request = NSMutableURLRequest(URL: NSURL(string: "http://api.petoye.com/users/signup")!)
+            request.HTTPMethod = "POST"
+            let postString = "email=user15@gmail.com&password=xcode123"
+            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+                guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                    print(error!)
+                    return
+                }
+                
+                if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 201 {           // check for http errors
+                    print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                    print(response!)
+                }
+                
+                let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print(responseString!)
+            }
+            task.resume()
+        }
+
         
             
     }
