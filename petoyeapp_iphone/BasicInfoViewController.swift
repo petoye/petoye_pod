8c7c7bab694dbc776481dc9b5f6a51c08c32b9d3
Ameya <ameyavi@gmail.com>
2016-07-29 04:42:05 +0530

    basic info scrolling done

--- petoyeapp_iphone/BasicInfoViewController.swift
+++ petoyeapp_iphone/BasicInfoViewController.swift
@@ -1,19 +1,115 @@
 import UIKit
 import CoreLocation
 
-class BasicInfoViewController: UIViewController, CLLocationManagerDelegate {
+class BasicInfoViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var done: UIButton!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var owner_type: UITextField!
     @IBOutlet weak var pet_type: UITextField!
     @IBOutlet weak var breed: UITextField!
+    @IBOutlet weak var ownertype: UIButton!
+    @IBOutlet weak var pettype: UIButton!
+    @IBOutlet weak var breedtype: UIButton!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var locationManager = CLLocationManager()
     var lat = Double()
     var long = Double()
     var currentLocation = CLLocation()
+    var pickerData: [String] = [String]()
+    var comp = Int()
+    var ro = Int()
     
+    @IBAction func ownertype(sender: AnyObject) {
+        picker.hidden = false
+        
+        pickerData = ["Pet Owner", "Pet Lover", "Pet Business"]
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        picker.center = CGPointMake(self.view.bounds.size.width / 2, self.view.frame.size.height/2 + 320 )
+        var bottomOffset: CGPoint = CGPointMake(0, self.scrollView.contentSize.height - self.scrollView.bounds.size.height)
+        self.scrollView.setContentOffset(bottomOffset, animated: true)
+        //print(comp)
+    }
+    
+    @IBAction func pettype(sender: AnyObject) {
+        
+        picker.hidden = false
+        pickerData = ["","","","Dog", "Cat", "Other"]
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        var bottomOffset: CGPoint = CGPointMake(0, self.scrollView.contentSize.height - self.scrollView.bounds.size.height)
+        self.scrollView.setContentOffset(bottomOffset, animated: true)
+        
+    }
+    
+    @IBAction func breedtype(sender: AnyObject) {
+        pickerData = ["","","","","","","Labrador", "Beagle", "Pug"]
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        var bottomOffset: CGPoint = CGPointMake(0, self.scrollView.contentSize.height - self.scrollView.bounds.size.height)
+        self.scrollView.setContentOffset(bottomOffset, animated: true)
+    }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    // Catpure the picker view selection
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+        if(row == 0)
+        {
+            owner_type.text = "Pet Owner"
+        }
+        else if(row == 1)
+        {
+            owner_type.text = "Pet Lover"
+        }
+        else if (row == 2)
+        {
+            owner_type.text = "Pet Business"
+        }
+        else if (row == 3)
+        {
+            pet_type.text = "Dog"
+        }
+        else if (row == 4)
+        {
+            pet_type.text = "Cat"
+        }
+        else if (row == 5)
+        {
+            pet_type.text = "Other"
+        }
+        else if (row == 6)
+        {
+            breed.text = "Labrador"
+        }
+        else if (row == 7)
+        {
+            breed.text = "Beagle"
+        }
+        else if (row == 8)
+        {
+            breed.text = "Pug"
+        }
+        
+    }
+ 
     @IBAction func done(sender: AnyObject) {
         
         var u_name = username.text!
