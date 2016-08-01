aa405ba780291fbfaa0eb3b3f56b792259bb072b
Ameya <ameyavi@gmail.com>
2016-08-01 16:33:29 +0530

    comments tab

---
+++ CommentsViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CommentsViewController.swift
+//  PetOye
+//
+//  Created by Ameya Vichare on 01/08/16.
+//  Copyright © 2016 Ameya Vichare. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    @IBOutlet weak var commentbox: UIView!
+    
+    @IBOutlet weak var comment: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+        comment.becomeFirstResponder()
+        self.hideKeyboardWhenTappedAround()
+        
+        
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            self.view.frame.origin.y -= keyboardSize.height
+            
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            self.view.frame.origin.y += keyboardSize.height
+           
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func commentBut(sender: AnyObject) {
+        print("pressed")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
