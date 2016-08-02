ac517890d58f78628de6dce3ad86f5e08b8f27a8
Ameya <ameyavi@gmail.com>
2016-08-02 21:38:26 +0530

    discover/follow done

--- petoyeapp_iphone/discover_cell.swift
+++ petoyeapp_iphone/discover_cell.swift
@@ -23,8 +23,8 @@ class discover_cell: UITableViewCell {
     var delegate: discoverDelegate?
     
     @IBAction func to_follow(sender: AnyObject) {
-        self.followedBut.viewWithTag(toFollowBut.tag)?.hidden = false
-        //self.toFollowBut.hidden = true
+        //self.followedBut.viewWithTag(toFollowBut.tag)?.hidden = false
+        print(toFollowBut.tag)
         self.toFollowBut.viewWithTag(toFollowBut.tag)?.hidden = true
         self.delegate?.reloadDiscover()
         
@@ -33,6 +33,48 @@ class discover_cell: UITableViewCell {
         //var FollowedUser_Id = data[toFollowBut.tag]
         //userDefault.setObject(FollowedUser_Id, forKey: "storedFollowedUserId")
         
+        // following a user api call
+        
+ 
+        let my_id = 1//userDefault.objectForKey("id")
+        //print("u_id=\(u_id)")
+        let his_id = userDefault.objectForKey("storedUserIds")
+        let hisid = his_id![toFollowBut.tag]
+        print(hisid)
+        
+        
+        // add a comment api call
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://api.petoye.com/\(my_id)/follow")!)
+        request.HTTPMethod = "POST"
+        let postString = "hisid=\(hisid)"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
+            guard error == nil && data != nil else {                                                          // check for fundamental networking error
+                print(error!)
+                return
+            }
+            
+            if let httpStat = response as? NSHTTPURLResponse where httpStat.statusCode == 201
+            {
+                // show followed
+                //self.toFollowBut.viewWithTag(self.toFollowBut.tag)?.hidden = true
+                // pop up followed
+            }
+            
+            
+            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 201 {           // check for http errors
+                print("statusCode should be 201, but is \(httpStatus.statusCode)")
+                print(response!)
+            }
+            
+            var responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print(responseString!)
+            
+            
+        }
+        task.resume()
+
         
     }
     
